b7cd1ec514a2e9fc0a065fddec28e6ca8b464fb5
AdityA <aditya.ace93@gmail.com>
2022-04-29 23:48:15 +0530

    player 1 added

--- main.py
+++ main.py
@@ -11,17 +11,38 @@ pygame.display.set_caption('Space Invader')
 icon = pygame.image.load('./img/ufo.png')
 pygame.display.set_icon(icon)
 
+# Player
+
+playerImg = pygame.image.load('./img/space-invaders.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 
 # Game Loop
 
 
 running = True
+
 while running:
+
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("Quitting Program")
             running = False
+
+    player()
+
+    pygame.display.update()
+
+
+
+
 
 
 
